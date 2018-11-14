32ce387609fc95f8b29a190a8939127129a02f91
Tony Ortega <tony.ortega@rallyhealth.com>
2018-11-14 12:51:17 -0600

    added a new method with contraband object

--- src/main/scala/api/schema/BreakingService.scala
+++ src/main/scala/api/schema/BreakingService.scala
@@ -1,9 +1,27 @@
 package api.schema
+import java.util.UUID.randomUUID
+
+import jdk.nashorn.internal.ir.RuntimeNode
+
+import scala.collection.immutable.Map
 
 class BreakingService {
 
 
   case class RsClaimSet(sub: String, sid: String, iat: String, breakingChange: String)
 
+  def getRequest: Request = {
+    val id = randomUUID().toString
+
+    val requestObject = Request(
+      domain = Domain.activity,
+      result = Map(
+        "id" -> id,
+        "activity" -> Activity.follow.toString
+      )
+    )
+
+    requestObject
+  }
 
 }
